e214bca2b2182dd15855b8a45d61a7c6ca82c56e
odusdegreat <tobiodubote@gmail.com>
2025-09-24 16:57:09 +0100

    feat: installed React, Typescript using vite

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,6 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "~": path.resolve(__dirname, "./node_modules"),
     },
   },
 }));
